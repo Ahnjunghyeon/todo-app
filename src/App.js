37f41674bc2091c5b9936bfa7c47043ff835f973
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2024-03-29 18:09:26 +0900

    [1]Create TodoItemInputField props

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
+const TodoItemInputField = (props) => {
+  return <div></div>;
+};
+
 function App() {
   return <div className="App"></div>;
 }
 
 export default App;
+
+// 순서
+// git add .
+// git commit -m " 실행 명 "
+// git pull origin main
+// git push origin main
