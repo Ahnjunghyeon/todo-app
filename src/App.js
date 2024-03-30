8a56c2dd04251f0eda219e3a9d34df9fa6f2059c
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2024-03-30 15:08:53 +0900

    [2] add TextField

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import "./App.css";
 
+import TextField from "@mui/material/TextField";
+
 const TodoItemInputField = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <TextField />
+    </div>
+  );
 };
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoItemInputField />
+    </div>
+  );
 }
 
 export default App;
 
 // 순서
-// git add .
-// git commit -m " 실행 명 "
-// git pull origin main
-// git push origin main
+// git status 로 변동된 점 확인
+// git add . 추가
+// git commit -m " 실행 명 " 추가
+// git pull origin main 깃에 올린 것 받아오기
+// git push origin main 깃에 올릴 것 올리기
