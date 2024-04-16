205e73a7168d99ead8dea213f17c24b19f7bb303
Ahnjunghyeon <https://github.com/Ahnjunghyeon>
2024-04-16 16:44:13 +0900

    4-16 error setting

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -13,20 +14,23 @@ import {
   doc,
   deleteDoc,
   getDocs,
+  query,
+  orderBy,
 } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-j8rkBZ9eFc7SNzspHT73dkT8MRINADA",
-  authDomain: "todo-list-app-d25b4.firebaseapp.com",
-  projectId: "todo-list-app-d25b4",
-  storageBucket: "todo-list-app-d25b4.appspot.com",
-  messagingSenderId: "662415885337",
-  appId: "1:662415885337:web:784ef3ef8b7dd47cc2d688",
-  measurementId: "G-HT6QKTGT9L",
+  apiKey: "AIzaSyAPjYrRKj11e85Yx1ZaqFiZ6STbH-ciUT8",
+  authDomain: "todo-list-app-98806.firebaseapp.com",
+  projectId: "todo-list-app-98806",
+  storageBucket: "todo-list-app-98806.appspot.com",
+  messagingSenderId: "886143434487",
+  appId: "1:886143434487:web:b5227108bd4261291f9df2",
+  measurementId: "G-NGJKZTHLQN",
 };
 
 // Initialize Firebase
@@ -34,9 +38,9 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
 
-//  함수를 정의하는 공간
 const TodoItemInputField = (props) => {
   const [input, setInput] = useState("");
+
   const onSubmit = () => {
     props.onSubmit(input);
     setInput("");
@@ -44,16 +48,13 @@ const TodoItemInputField = (props) => {
 
   return (
     <div>
-      {/* 1. 텍스트가 입력될 창 만들기. */}
       <TextField
         id="todo-item-input"
-        lable="Todo Item"
+        label="Todo Item"
         variant="outlined"
-        value={input}
-        // onChange = 바꿔라, e(이벤트)가 일어날때 setInput을 value로
         onChange={(e) => setInput(e.target.value)}
+        value={input}
       />
-      {/* 2. 입력한 것을 저장할 버튼 생성 */}
       <Button variant="outlined" onClick={onSubmit}>
         Submit
       </Button>
@@ -62,7 +63,6 @@ const TodoItemInputField = (props) => {
 };
 
 const TodoItem = (props) => {
-  // 체크하면 끝내는 코드
   const style = props.todoItem.isFinished
     ? { textDecoration: "line-through" }
     : {};
@@ -81,12 +81,11 @@ const TodoItem = (props) => {
   );
 };
 
-// 버튼으로 입력한 내용들이 저장될 컴포넌트
 const TodoItemList = (props) => {
   const todoList = props.todoItemList.map((todoItem, index) => {
     return (
       <TodoItem
-        key={todoItem.id}
+        key={index}
         todoItem={todoItem}
         onTodoItemClick={props.onTodoItemClick}
         onRemoveClick={props.onRemoveClick}
@@ -100,58 +99,48 @@ const TodoItemList = (props) => {
   );
 };
 
-let todoItemId = 0;
-
 function App() {
   const [todoItemList, setTodoItemList] = useState([]);
 
-  // 앱을 새로고침해도 내용들이
   const syncTodoItemListStateWithFirestore = () => {
-    getDocs(collection(db, "todoItem")).then((querySnapshot) => {
+    const q = query(collection(db, "todoItem"), orderBy("createdTime", "desc"));
+
+    getDocs(q).then((querySnapshot) => {
       const firestoreTodoItemList = [];
       querySnapshot.forEach((doc) => {
         firestoreTodoItemList.push({
           id: doc.id,
-          // todoItemContent 함수에 리스트로 저장되어서 새로고침해도 보이게
           todoItemContent: doc.data().todoItemContent,
           isFinished: doc.data().isFinished,
+          createdTime: doc.data().createdTime ?? 0,
         });
       });
       setTodoItemList(firestoreTodoItemList);
     });
   };
+
   useEffect(() => {
     syncTodoItemListStateWithFirestore();
   }, []);
-
   const onSubmit = async (newTodoItem) => {
     await addDoc(collection(db, "todoItem"), {
       todoItemContent: newTodoItem,
-      inFinished: false,
+      isFinished: false,
+      createdTime: Math.floor(Date.now() / 1000),
     });
     syncTodoItemListStateWithFirestore();
-    setTodoItemList([
-      ...todoItemList,
-      {
-        id: todoItemId++,
-        todoItemContent: newTodoItem,
-        isFinished: false,
-      },
-    ]);
   };
 
-  //  입력된 값을 map을통해서 리스트로 변환해줌
   const onTodoItemClick = async (clickedTodoItem) => {
     const todoItemRef = doc(db, "todoItem", clickedTodoItem.id);
     await setDoc(
       todoItemRef,
-      { isFiniished: !clickedTodoItem.isFinished },
+      { isFinished: !clickedTodoItem.isFinished },
       { merge: true }
     );
     syncTodoItemListStateWithFirestore();
   };
 
-  // 지우는 함수
   const onRemoveClick = async (removedTodoItem) => {
     const todoItemRef = doc(db, "todoItem", removedTodoItem.id);
     await deleteDoc(todoItemRef);
@@ -160,12 +149,10 @@ function App() {
 
   return (
     <div className="App">
-      {/* 3.  */}
       <TodoItemInputField onSubmit={onSubmit} />
       <TodoItemList
         todoItemList={todoItemList}
         onTodoItemClick={onTodoItemClick}
-        // 지우는 코드
         onRemoveClick={onRemoveClick}
       />
     </div>
@@ -173,10 +160,3 @@ function App() {
 }
 
 export default App;
-
-// 순서
-// git status 로 변동된 점 확인
-// git add . 추가
-// git commit -m " 실행 명 " 추가
-// git pull origin main 깃에 올린 것 받아오기
-// git push origin main 깃에 올릴 것 올리기
